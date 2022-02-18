68577c0feec1bd618d99cac678f6c82d6ac3cecd
indrajith76 <indrajithgoswami@gmail.con>
2022-02-18 16:20:47 +0600

    WorkWithLayout

--- BookStore/Startup.cs
+++ BookStore/Startup.cs
@@ -27,6 +27,8 @@ namespace BookStore
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddRazorPages().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
