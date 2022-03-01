1077f0e2c73552c8fde4c74d5e5059700ac6b51b
indrajith76 <indrajithgoswami@gmail.con>
2022-03-02 01:56:00 +0600

    setup DbContext class in entity framework core

---
+++ BookStore/Data/BookStoreContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Data
+{
+    public class BookStoreContext : DbContext
+    {
+        public BookStoreContext(DbContextOptions<BookStoreContext> options)
+            :base(options)
+        {
+                
+        }
+        public DbSet<Books> Books { get; set; }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer("Server=.;Database=BookStore;Integrated Security=True;");
+        //    base.OnConfiguring(optionsBuilder);
+        //}
+    }
+}
