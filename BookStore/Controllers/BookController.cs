e4c956badf36ab24a23fde4fee81e31bbadee913
indrajith76 <indrajithgoswami@gmail.con>
2022-03-05 17:53:09 +0600

    Making Async call using entity framework core

--- BookStore/Controllers/BookController.cs
+++ BookStore/Controllers/BookController.cs
@@ -41,9 +41,9 @@ namespace BookStore.Controllers
         }
 
         [HttpPost]
-        public IActionResult AddNewBook(BookModel bookModel)
+        public async Task<IActionResult> AddNewBook(BookModel bookModel)
         {
-            int id = _bookRepository.AddNewBook(bookModel);
+            int id = await _bookRepository.AddNewBook(bookModel);
             if(id > 0)
             {
                 return RedirectToAction(nameof(AddNewBook), new { isSuccess = true, bookId = id});
