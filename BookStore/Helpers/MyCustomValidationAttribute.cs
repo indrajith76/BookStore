6274c5256f99d70867aa28cf77a1d45eda8ced07
indrajith76 <indrajithgoswami@gmail.con>
2022-03-12 19:48:22 +0600

    Custom Validation Attribute in asp.net core(1)

---
+++ BookStore/Helpers/MyCustomValidationAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Helpers
+{
+    public class MyCustomValidationAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value != null)
+            {
+                string bookName = value.ToString();
+                if (bookName.Contains("mvc"))
+                {
+                    return ValidationResult.Success;
+                }
+
+            }
+            return new ValidationResult("BookName does not contain the desired value");
+        }
+    }
+}
--- BookStore/Models/BookModel.cs
+++ BookStore/Models/BookModel.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using BookStore.Helpers;
 
 namespace BookStore.Models
 {
     public class BookModel
     {
         public int Id { get; set; }
-        [StringLength(100, MinimumLength = 5)]
-        [Required(ErrorMessage ="Please enter the title of your book ")]
+        //[StringLength(100, MinimumLength = 5)]
+        //[Required(ErrorMessage ="Please enter the title of your book ")]
+        [MyCustomValidation]
         public string Title { get; set; }
         [Required(ErrorMessage ="Please enter the author name")]
         public string Author { get; set; }
